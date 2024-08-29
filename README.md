5ef467f47876b9e068559531bed4675cfc4a82f2
Henriquedearaujo <ricke_cris@hotmail.com>
2024-08-29 18:43:20 -0300

    teste usando jest com atributo data-testid

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
+# Exercicio Teste Unitario usando react testing library.
+
+## Para o exercício
+O arquivo onde o teste deve ser escrito está em src/components/PostComments/PostComments.test.tsx
+
+1) `Crie um código para testar a inserção de dois comentários;`
+
+2) `Utilize o atributo data-testid;`
+
+3) `Atualize o repositório com o teste criado;`
+
 # Requisitos
 ## Instalar as dependências
 `npm install` ou `yarn install`
@@ -8,8 +19,4 @@
 ## Para executar a aplicação em localhost
 `npm start` ou `yarn start`
 
-# Para o exercício
-O arquivo onde o teste deve ser escrito está em src/components/PostComments/PostComments.test.tsx
 
-## Resolução
-Neste repositório existe uma branch chamada **resolucao** com o exercício resolvido, é importante que a utilize como consulta caso tenha dificuldades ou até mesmo para comparar com a abordagem que você adotou, **não faça simplesmente um copiar/colar** - confio em você ;)
--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,29 @@
+
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+        render(<PostComment />);
+        expect(screen.getByTestId('submit-button')).toBeInTheDocument();
+    });
+
+    it('Deve permitir a inserção de dois comentários', () => {
+        render(<PostComment />);
+
+        const commentInput = screen.getByTestId('comment-input');
+        const submitButton = screen.getByTestId('submit-button');
+
+        fireEvent.change(commentInput, { target: { value: 'comentário 1' } });
+        fireEvent.click(submitButton);
+
+        expect(screen.getAllByTestId('comment-item').length).toBe(1);
+        expect(screen.getByText('comentário 1')).toBeInTheDocument();
+
+        fireEvent.change(commentInput, { target: { value: ' comentário 2 ' } });
+        fireEvent.click(submitButton);
+
+        expect(screen.getAllByTestId('comment-item').length).toBe(2);
+        expect(screen.getByText('comentário 2')).toBeInTheDocument();
     });
 });
